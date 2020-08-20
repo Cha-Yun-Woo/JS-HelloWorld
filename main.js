bedda43e8463a3264eef61bfa996da18091f450a
차윤우 <chayunu10@gmail.com>
2020-08-20 12:28:13 +0900

    이이잉 앗살람알라이쿰 외 3가지 멘트 추가

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 console.log("안녕하세요 잼민TV의 재민입니다. 오늘은 국밥에 대해 알아볼건데요. 국밥은 맛있습니다. 이 영상이 도움되셨다면 구독과 좋아요 알림설정까지 해주세요.")
+console.log("이이이잉 앗살람알라이쿰")
+console.log("명령어는 시바 git push로 업로드 하는 것이여")
+console.log("git add . 치고")
+console.log("git commit -a -m \"쓸말\" 써서 멘트 추가하라")
